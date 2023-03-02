2285c55fb7072178116c5e90edded0615f9b75fd
Haydar Alçin <hydralcin@gmail.com>
2023-03-02 18:35:39 +0300

    v0.2

--- RockPaperScissors-Simulator/Assets/Scripts/MoveController.cs
+++ RockPaperScissors-Simulator/Assets/Scripts/MoveController.cs
@@ -8,35 +8,139 @@ public class MoveController : MonoBehaviour
     public string rock, paper, scissors;
 
     public float speed;
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        // tag'e sahip olan tüm nesneleri al
-        GameObject[] paper = GameObject.FindGameObjectsWithTag(rock);
 
-        // nesneleri uzaklýklarýna göre sýrala
-        GameObject closest = paper.OrderBy(obj => Vector2.Distance(transform.position, obj.GetComponent<Transform>().position)).FirstOrDefault();
+        if (this.gameObject.tag==paper)
+        {
+            if (GameObject.FindGameObjectsWithTag(rock).Length>0)
+            {
+                //tag'e sahip olan tüm nesneleri al
+                GameObject[] paper = GameObject.FindGameObjectsWithTag(rock);
 
-        Vector2 closestPosi = closest.transform.position;
-
-        Vector2 newPosition = Vector2.Lerp(transform.position, closestPosi, speed * Time.deltaTime);
-
-        // player nesnesini yeni pozisyona taþý
-        transform.position = newPosition;
+                //nesneleri uzaklýklarýna göre sýrala
+                GameObject closestRock = paper.OrderBy(obj => Vector2.Distance(transform.position, obj.GetComponent<Transform>().position)).FirstOrDefault();
 
 
+                //Targettan kendi pozisyonumuza bir lerp vectorü
+                Vector2 newPosition = Vector2.MoveTowards(transform.position, closestRock.transform.position, speed * Time.deltaTime);
+                // player nesnesini yeni pozisyona taþý
+                transform.position = newPosition;
+            }
+
+            
+        }
+        
+        else if (this.gameObject.tag == rock)
+        {
+            if (GameObject.FindGameObjectsWithTag(scissors).Length>0)
+            {
+                // tag'e sahip olan tüm nesneleri al
+                GameObject[] rock = GameObject.FindGameObjectsWithTag(scissors);
+
+                // nesneleri uzaklýklarýna göre sýrala
+                GameObject closestScissors = rock.OrderBy(obj => Vector2.Distance(transform.position, obj.GetComponent<Transform>().position)).FirstOrDefault();
+
+
+                //Targettan kendi pozisyonumuza bir lerp vectorü
+                Vector2 newPosition = Vector2.MoveTowards(transform.position, closestScissors.transform.position, speed * Time.deltaTime);
+                // player nesnesini yeni pozisyona taþý
+                transform.position = newPosition;
+            }
+
+            
+        }
+
+        else if (this.gameObject.tag == scissors)
+        {
+            if (GameObject.FindGameObjectsWithTag(paper).Length>0)
+            {
+                // tag'e sahip olan tüm nesneleri al
+                GameObject[] scissors = GameObject.FindGameObjectsWithTag(paper);
+
+                // nesneleri uzaklýklarýna göre sýrala
+                GameObject closestPaper = scissors.OrderBy(obj => Vector2.Distance(transform.position, obj.GetComponent<Transform>().position)).FirstOrDefault();
+                
+
+                //Targettan kendi pozisyonumuza bir lerp vectorü
+                Vector2 newPosition = Vector2.MoveTowards(transform.position, closestPaper.transform.position, speed * Time.deltaTime);
+                // player nesnesini yeni pozisyona taþý
+                transform.position = newPosition;
+            }
+
+            
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "rock")
+        if (this.gameObject.tag==paper)
         {
-            Destroy(collision.gameObject);
+            if (collision.tag==rock)
+            {
+                collision.GetComponent<SpriteRenderer>().sprite = this.gameObject.GetComponent<SpriteRenderer>().sprite;
+                collision.tag = paper;
+            }
+
+        }
+
+        else if (this.gameObject.tag == rock)
+        {
+            if (collision.tag == scissors)
+            {
+                collision.GetComponent<SpriteRenderer>().sprite = this.gameObject.GetComponent<SpriteRenderer>().sprite;
+                collision.tag = rock;
+
+            }
+
+        }
+
+        else if (this.gameObject.tag == scissors)
+        {
+            if (collision.tag == paper)
+            {
+                collision.GetComponent<SpriteRenderer>().sprite = this.gameObject.GetComponent<SpriteRenderer>().sprite;
+                collision.tag = scissors;
+
+            }
+
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (this.gameObject.tag == paper)
+        {
+            if (collision.tag == rock)
+            {
+                collision.GetComponent<SpriteRenderer>().sprite = this.gameObject.GetComponent<SpriteRenderer>().sprite;
+                collision.tag = paper;
+            }
+
+        }
+
+        else if (this.gameObject.tag == rock)
+        {
+            if (collision.tag == scissors)
+            {
+                collision.GetComponent<SpriteRenderer>().sprite = this.gameObject.GetComponent<SpriteRenderer>().sprite;
+                collision.tag = rock;
+
+            }
+
+        }
+
+        else if (this.gameObject.tag == scissors)
+        {
+            if (collision.tag == paper)
+            {
+                collision.GetComponent<SpriteRenderer>().sprite = this.gameObject.GetComponent<SpriteRenderer>().sprite;
+                collision.tag = scissors;
+
+            }
+
         }
     }
 }
